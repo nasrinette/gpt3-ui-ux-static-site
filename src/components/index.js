f91582c2d614a4b477833bc7270bd9ee06d6b4e2
nasrinette <97260770+nasrinette@users.noreply.github.com>
2022-12-27 01:52:40 +0400

    Modern ui/ux gpt3

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article} from './article/article'
+export {default as Feature} from './feature/feature'
+export {default as Brand} from './brand/brand'
+export {default as CTA} from './cta/cta'
+export {default as Navbar} from './navbar/navbar'
